7479a362949286990b9f0c47beebf82816ec7513
Daniel Bernar <dbcongard@gmail.com>
2019-12-15 00:08:30 +0200

    Fix #3

--- src/utilization.cpp
+++ src/utilization.cpp
@@ -185,10 +185,10 @@ void MemoryUtilizationWorker::receiveData() {
     std::vector<std::string> lines = split(exec(NVSMI_CMD_MEM_UTILIZATION), "\n"), data;
     for (size_t i = 1; i < lines.size() - 1; i++) {
         data = split(lines[i], ", ");
-        udata[i - 1].level = std::atoi(split(data[0], " ")[0].c_str());
         memoryData[i - 1].total = std::atoi(split(data[1], " ")[0].c_str());
         memoryData[i - 1].free = std::atoi(split(data[2], " ")[0].c_str());
         memoryData[i - 1].used = std::atoi(split(data[3], " ")[0].c_str());
+        udata[i - 1].level = memoryData[i - 1].used * 100 / memoryData[i - 1].total;
     }
 }
 
