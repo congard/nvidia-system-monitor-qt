0acdc974781da140bcbc5ae46784f324f2d68fe8
hrdkjain@cordelia-ubuntu <hrdkjain@gmail.com>
2020-07-15 14:35:04 +0200

    Modified: processes to show GPU Memory Use in MB but removes sm, enc and dec

--- src/processes.cpp
+++ src/processes.cpp
@@ -8,18 +8,14 @@
 #include "utils.h"
 
 ProcessList::ProcessList(const std::string& name, const std::string& type,
-                         const std::string& gpuIdx, const std::string& pid,
-                         const std::string& sm, const std::string& mem,
-                         const std::string& enc, const std::string& dec)
+                const std::string& gpuIdx, const std::string& pid,
+                const std::string& mem)
 {
     this->name = name;
     this->type = type;
     this->gpuIdx = gpuIdx;
     this->pid = pid;
-    this->sm = sm;
     this->mem = mem;
-    this->enc = enc;
-    this->dec = dec;
 }
 
 void ProcessesWorker::work() {
@@ -34,10 +30,11 @@ void ProcessesWorker::work() {
         data = split(lines[i], " ");
         
         processes.emplace_back(
-            data[NVSMI_NAME], data[NVSMI_TYPE],
-            data[NVSMI_GPUIDX], data[NVSMI_PID],
-            data[NVSMI_SM], data[NVSMI_MEM],
-            data[NVSMI_ENC], data[NVSMI_DEC]
+            data[NVSMI_NAME],
+            data[NVSMI_TYPE],
+            data[NVSMI_GPUIDX],
+            data[NVSMI_PID],
+            data[NVSMI_MEM]
         );
     }
     
@@ -61,14 +58,11 @@ ProcessesTableView::ProcessesTableView(QWidget *parent) : QTableView(parent) {
  
     // Column titles
     QStringList horizontalHeader;
-    horizontalHeader.append("Name");
+    horizontalHeader.append("Name of Process"); // Longer header increases column width so the process names are visible
     horizontalHeader.append("Type (C/G)");
     horizontalHeader.append("GPU ID");
     horizontalHeader.append("Process ID");
-    horizontalHeader.append("Compute use");
-    horizontalHeader.append("GPU Memory Use");
-    horizontalHeader.append("Encoding");
-    horizontalHeader.append("Decoding");
+    horizontalHeader.append("GPU Memory Use (MB)");
     
     model->setHorizontalHeaderLabels(horizontalHeader);
     
@@ -134,10 +128,7 @@ void ProcessesTableView::onDataUpdated() {
         _setItem(i, NVSM_TYPE, type);
         _setItem(i, NVSM_GPUIDX, gpuIdx);
         _setItem(i, NVSM_PID, pid);
-        _setItem(i, NVSM_SM, sm);
-        _setItemExt(i, NVSM_MEM, mem, " MB");
-        _setItem(i, NVSM_ENC, enc);
-        _setItem(i, NVSM_DEC, dec);
+        _setItem(i, NVSM_MEM, mem);
     }
     
     int index = worker->processesIndexByPid(selectedPid);
