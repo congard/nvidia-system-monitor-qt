ecd319fe5ebe272bf8e480f0012c70a98113d0b8
Daniel Bernar <dbcongard@gmail.com>
2020-08-08 16:55:20 +0300

    Move html code to the separate files

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -16,6 +16,7 @@
 #include "utilization/gpu/GPUUtilizationWorker.h"
 #include "utilization/memory/MemoryUtilizationWorker.h"
 
+#include "resources/MainWindowResources.h"
 #include "constants.h"
 
 MainWindow::MainWindow(QWidget*) {
@@ -80,45 +81,12 @@ void MainWindow::closeEvent(QCloseEvent *event) {
 }
 
 void MainWindow::about() {
-    QMessageBox::information(
-        nullptr,
-        "About",
-        R"(<font size=4><b>NVIDIA System Monitor</b></font>
-        <br>Version )" QNVSM_VERSION
-        R"(<br>The app monitors your Nvidia GPU
-        <br><br>Developed by Daniel Bernar
-        <br><a href='dbcongard@gmail.com'>dbcongard@gmail.com</a>
-        <br><br><a href='https://github.com/congard/nvidia-system-monitor-qt/blob/master/DONATE.md'>Donate</a> <a href='https://github.com/congard/nvidia-system-monitor-qt'>GitHub</a> <a href='https://t.me/congard'>Telegram</a>)"
-    );
+    QMessageBox::information(nullptr, "About", Resources::MainWindow::About);
 }
 
 void MainWindow::help() {
     QMessageBox msgBox;
     msgBox.setText("<font size=4><b>Help</b></font>");
-    msgBox.setInformativeText(
-        R"(<b>Settings</b><br>By default, update delay is 2 seconds (2000 ms).
-        You most likely want to change this value to, for example, 500 ms. To do this, create file <i>config</i> in the folder
-        <i>~/.config/nvidia-system-monitor</i>
-        <br><br><b>config values</b>
-        <ul>
-            <li>updateDelay &lt;time in ms&gt;</li>
-            <li>graphLength &lt;time in ms&gt;</li>
-            <li>gpuColor &lt;gpu index&gt; &lt;red&gt; &lt;green&gt; &lt;blue&gt;</li>
-        </ul><br>
-        <b>Processes</b>
-        <ul>
-            <li>Name - process name</li>
-            <li>Type - "C" for Compute Process, "G" for Graphics Process, and "C+G" for the process having both Compute and Graphics contexts</li>
-            <li>GPU ID - id of GPU in which process running</li>
-            <li>pid - process id</li>
-            <li>sm [%]</li>
-            <li>mem [%]</li>
-            <li>enc [%]</li>
-            <li>dec [%]</li>
-        </ul><br>
-        <b>GPU Utilization</b><br>This section displays a graph of gpu utilization.
-        <br><br><b>Memory Utilization</b><br>This section displays a graph of memory utilization.
-        <br><br><a href='https://github.com/congard/nvidia-system-monitor-qt/blob/master/DONATE.md'>Donate</a> <a href='https://github.com/congard/nvidia-system-monitor-qt'>GitHub</a> <a href='https://t.me/congard'>Telegram</a>)"
-    );
+    msgBox.setInformativeText(Resources::MainWindow::Help);
     msgBox.exec();
 }
